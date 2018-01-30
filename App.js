293363e06973b42aeda7486b53ed992a7271679c
Adrian Hall <adrian@shellmonger.com>
2018-01-30 09:37:31 -0800

    Initial Version

--- App.js
+++ App.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/es/integration/react';
+import { navigatorConfig } from './src/screens';
+import { persistor, store } from './src/redux/store';
 
 export default class App extends React.Component {
   render() {
+    const Navigator = StackNavigator(navigatorConfig);
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <Navigator/>
+        </PersistGate>
+      </Provider>
     );
   }
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
